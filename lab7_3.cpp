c0acef9cd260cddf4a96e033d92ee1acee766634
notname <notmy3maildlaoalsdasda@gmail.com>
2022-12-13 18:13:52 +0700

    Done

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,42 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
 
+//การเติมคำตอบในช่องนี้ ขอให้เติมเฉพาะ Function adiff() เท่านั้น โดยห้ามเติม Function main() มาในนี้
+
+
+//การเติมคำตอบในช่องนี้ ขอให้เติมเฉพาะ Function adiff() เท่านั้น โดยห้ามเติม Function main() มาในนี้
+
+int adiff(int a,int b) {
+
+         
+
+        
+         
+         if (abs(a-b)%360 > 180) {
+         
+         return 360-abs(a-b)%360; }
+         
+          return abs(a-b)%360;
+    
+}
+
+
+
+
+
+
+
+
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244) << endl;
 }
