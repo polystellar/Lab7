f09297114e7fd1bf12edd9eb8800148b13bbb1b0
notname <notmy3maildlaoalsdasda@gmail.com>
2022-12-13 16:48:57 +0700

    d0ne

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -9,6 +9,42 @@ int main(){
 		P[i] = 2*i+1;
 		i++;
 	}
+
+	cout << "Please input k: ";
+	cin >> k;
+
+	if (k > 0) {
+	
+	i = 0;
+	result = 0;
+
+	while (i<5000) {
+
+		if ( P[i] % k == 0) {
+
+			result -= P[i];
+		}
+		else {
+			result += P[i];
+		}
+		i++;
+
+
+
+	}
+
+	if (i>=5000) {
+
+		cout << "Result = " << result;
+	}
+
+
+	}
+
+	else {
+
+		cout << "Invalid input!!!";
+	}
 	
 	return 0;
 }
