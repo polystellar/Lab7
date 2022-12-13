cef548e77a28bcb1f6efaeac00814fa9822ea8af
notname <notmy3maildlaoalsdasda@gmail.com>
2022-12-13 16:11:51 +0700

    Done

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -4,12 +4,27 @@
 using namespace std;
 
 int main(){
+
+	double gpasearch;
 	
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
     //Write your code here.
+	cout << "Please input GPA: ";
+	cin >> gpasearch;
+
+	for (int i = 0 ; i < 10 ; i++) {
+
+		if (gpasearch <= gpa[i]) {
+
+			cout << id[i] << " "<< name[i] << " " << gpa[i] << endl;
+		}
+
+
+	}
+
 		
 	return 0;
 }
